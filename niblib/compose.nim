44bca17cd56a0f3022cd1d61b817b0ff50d69fe5
Zev Kronenberg <zkronenberg@pacificbiosciences.com>
2020-10-11 13:40:21 -0700

    builds, added cligen

--- niblib/compose.nim
+++ niblib/compose.nim
@@ -1,28 +1,27 @@
 import hts
 
+proc retrieve_flanking_sequences_from_fai(fastaIdx: Fai, chrom: string,
+        start_pos, end_pos, flank: int) =
+    ## this function lacks a return
+    var five_prime_seq = fastaIdx.get(chrom, start_pos - flank, start_pos)
+    var three_prime_seq = fastaIdx.get(chrom, end_pos, end_pos + flank)
 
 
-
-
-## Takes in a FAI object and returns a pair of strings, covering the flanks of chrom[start_pos - flank, start_pos]
-## to chrom[end_pos, end_pos + flank]
-proc retrieve_flanking_sequences_from_fai(fai : Fai, chrom : string, start_pos : int, end_pos : int, flank : int):
-    var five_prime_seq = fai.get(chrom, start_pos - flank, start_pos)
-    var three_prime_seq = fai.get(chrom, end_pos, end_pos + flank)
-
-    return five_prime_seq, three_prime_seq
-
-
-proc compose_variants(variant_file : string, reference_file : string):
-
-    var variants:VCF
+proc compose_variants(variant_file: string, reference_file: string) =
+    ## function to compose
+    var variants: VCF
     doAssert(open(variants, variant_file))
 
     for v in variants:
-        info = v.info
+        var infoF = v.info
         ## Extract SV type / SV END / start position
-        
+
         ## Retrieve flanks, either from FAI or string cache
-        
+
         ## Generate a single sequence from variant seq + flank,
         ## taking into account the variant type.
+
+
+when isMainModule:
+    import cligen
+    dispatch(compose_variants)
