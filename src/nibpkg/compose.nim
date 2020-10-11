1aaf6d805d394d234dafb74adab7841b3634b045
Eric T. Dawson <eric.t.dawson@gmail.com>
2020-10-11 18:27:35 -0400

    [compose] Add return vlaue (list) for compose_variants

--- src/nibpkg/compose.nim
+++ src/nibpkg/compose.nim
@@ -13,13 +13,28 @@ proc retrieve_flanking_sequences_from_fai(fastaIdx: Fai, chrom: string,
 
 proc compose(variant: Variant, right_flank: string,
     left_flank: string): string =
-  var combined_sequence = right_flank & left_flank
+
+  var inner_seq: string
+  var variant_type: string
+  doAssert variant.info.get("SVTYPE", variant_type) == Status.OK
+  if variant_type == "DEL":
+    inner_seq = ""
+  elif variant_type == "INS":
+    inner_seq = variant.ALT[0]
+  elif variant_type == "INV":
+    raise newException(ValueError,
+    "Error: Inversion processing not implemented.")
+  var combined_sequence = right_flank & inner_seq & left_flank
 
   return combined_sequence
 
 
 proc compose_variants*(variant_file: string, reference_file: string) =
   ## function to compose variants from their sequence / FASTA flanking regions
+  ## Returns a Sequence of strings representing the DNA sequence of the flanking
+  ## regions and variant sequence.
+
+  var composed_seqs = newSeq[string](1)
 
   ## Open FASTA index
   var fai: Fai
@@ -40,7 +55,10 @@ proc compose_variants*(variant_file: string, reference_file: string) =
         v.start), int(v.stop), 100)
     ## Generate a single sequence from variant seq + flank,
     ## taking into account the variant type.
+    var variant_seq = compose(v, flanks.right, flanks.left)
+    composed_seqs.add(variant_seq)
 
+  return composed_seqs
 
 when isMainModule:
   import cligen
