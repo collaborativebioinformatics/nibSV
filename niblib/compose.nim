2f133aa39f450d1455d7ad8241427977a214ad3b
Eric T. Dawson <eric.t.dawson@gmail.com>
2020-10-11 17:06:40 -0400

    [compose] Prettify compose.nim

--- niblib/compose.nim
+++ niblib/compose.nim
@@ -5,37 +5,37 @@ type
     left, right: string
 
 proc retrieve_flanking_sequences_from_fai(fastaIdx: Fai, chrom: string,
-        start_pos, end_pos, flank: int) : FlankSeq  =
-    ## this function lacks a return
-    result.left = fastaIdx.get(chrom, start_pos - flank, start_pos)
-    result.right = fastaIdx.get(chrom, end_pos, end_pos + flank)
+        start_pos, end_pos, flank: int): FlankSeq =
+  ## this function lacks a return
+  result.left = fastaIdx.get(chrom, start_pos - flank, start_pos)
+  result.right = fastaIdx.get(chrom, end_pos, end_pos + flank)
 
 
 
 proc compose_variants(variant_file: string, reference_file: string) =
-    ## function to compose
+  ## function to compose
 
-    ## Open FASTA index
-    var fai:Fai
-    if not fai.open(reference_file):
-        quit ("Failed to open FASTA file: " & reference_file)
+  ## Open FASTA index
+  var fai: Fai
+  if not fai.open(reference_file):
+    quit ("Failed to open FASTA file: " & reference_file)
 
-    var variants: VCF
-    doAssert(open(variants, variant_file))
+  var variants: VCF
+  doAssert(open(variants, variant_file))
 
-    var sv_end : int32
-    var sv_start : int32
-    var sv_type : string
-    for v in variants:
-        var info_fields = v.info
-        ## Extract SV type / SV END / start position
+  var sv_end: int32
+  var sv_start: int32
+  var sv_type: string
+  for v in variants:
+    var info_fields = v.info
+    ## Extract SV type / SV END / start position
+    info_fields.get("SVTYPE", sv_type)
+    ## Retrieve flanks, either from FAI or string cache
 
-        ## Retrieve flanks, either from FAI or string cache
-
-        ## Generate a single sequence from variant seq + flank,
-        ## taking into account the variant type.
+    ## Generate a single sequence from variant seq + flank,
+    ## taking into account the variant type.
 
 
 when isMainModule:
-    import cligen
-    dispatch(compose_variants)
+  import cligen
+  dispatch(compose_variants)
