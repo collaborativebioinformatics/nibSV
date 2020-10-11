838b5b211374d202685e6ac65c820f7b8e108018
Eric T. Dawson <eric.t.dawson@gmail.com>
2020-10-11 16:56:04 -0400

    [compose] Add FASTA file error handline to compose_variants.

--- niblib/compose.nim
+++ niblib/compose.nim
@@ -1,27 +1,32 @@
 import hts
 
+
+
 proc retrieve_flanking_sequences_from_fai(fastaIdx: Fai, chrom: string,
-        start_pos, end_pos, flank: int) =
+        start_pos, end_pos, flank: int): tuple =
     ## this function lacks a return
     var five_prime_seq = fastaIdx.get(chrom, start_pos - flank, start_pos)
     var three_prime_seq = fastaIdx.get(chrom, end_pos, end_pos + flank)
 
+    var flanks : tuple[five_prime : string, three_prime : string]
+
+    return flanks
+
+
 
 proc compose_variants(variant_file: string, reference_file: string) =
     ## function to compose
+    
+    ## Open FASTA index
+    var fai:Fai
+    if not fai.open(reference_file):
+        quit ("Failed to open FASTA file: " & reference_file)
+    
     var variants: VCF
     doAssert(open(variants, variant_file))
 
-    var sv_type = new_string_of_cap(20)
-    var sv_len = int32
-    var sv_end = int32
     for v in variants:
-
-        infoF = v.info
-        do infoF.get("SVEND")
-        ## Extract SV type / SV END / start position
-
-
+        var info_fields = v.info
         ## Extract SV type / SV END / start position
 
         ## Retrieve flanks, either from FAI or string cache
