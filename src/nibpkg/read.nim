bfa595d55cd55f51dc19cb3e476d1d1c2fee9ba4
Eric T. Dawson <eric.t.dawson@gmail.com>
2020-10-12 15:53:03 -0400

    Add filter function for read matches. Could be better implemented with some functional programming on the compatible_SVs CountTable.

--- src/nibpkg/read.nim
+++ src/nibpkg/read.nim
@@ -12,7 +12,24 @@ proc process_read(s : string, idx : svIdx, k:int=25): Read =
     for kmer in l.seeds:
         var compats = idx.lookupKmer(kmer)
         for c in compats:
-            x.mget(compatible_SVs[c]).inc
+            x.mgetOrPut(compatible_SVs[c], 0).inc
 
     return x
 
+proc filter_read_matches(read : Read, min_matches:int=2, winner_takes_all:bool=false) =
+    
+    var removables : seq[uint32]
+    var max_key:uint32=0
+    var max_val:uint32=0
+    for key, val in read.compatible_SVs:
+        if val > min_matches:
+            removables.add(key)
+        if val > max_val:
+            max_key = key
+            max_val = val
+    for r in removables:
+        compatible_SVs.del(r)
+    
+    if winner_takes_all:
+        clear(compatible_SVs)
+        compatible_SVs[max_key] = val
