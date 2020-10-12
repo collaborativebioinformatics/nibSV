cae19bfc529ff03088910d8180ead01da195cfc1
Christopher Dunn <cdunn@pacificbiosciences.com>
2020-10-12 17:42:00 -0500

    minor cleanup

--- src/nibpkg/refmers.nim
+++ src/nibpkg/refmers.nim
@@ -4,34 +4,30 @@ import tables
 import svidx
 
 type
-    Chunk* = object
+    Chunk = object
         chrom_name: string
         chrom_start: int
         chrom_end: int
     
-proc addRefCount*(refKmers: CountTableRef[uint64], svKmers: svIdx) =
+proc addRefCount(refKmers: CountTableRef[uint64], svKmers: svIdx) =
     for k, v in pairs(refKmers): 
         if svKmers.hasKey(k):
             svKmers[k].refCount += uint32(v)
 
-iterator createdChunks*(fai: Fai, chunk_size: int): Chunk =
+iterator createdChunks(fai: Fai, chunk_size: int): Chunk =
     for i in 0..<fai.len:
         let chrom_name = fai[i]
         let chrom_len = fai.chrom_len(chrom_name)
         for j in countup(0, chrom_len, chunk_size):
-            var chunk: Chunk
-            chunk.chrom_name = chrom_name
-            chunk.chrom_start = j
-            chunk.chrom_end = j + chunk_size
-            yield chunk
+            yield Chunk(chrom_name:chrom_name, chrom_start:j, chrom_end: j + chunk_size)
 
-proc buildKmerCountTable*(full_sequence: string, kmer_size: int=21):CountTableRef[uint64] =
+proc buildKmerCountTable(full_sequence: string, kmer_size: int=21):CountTableRef[uint64] =
     let convertedKmers: pot_t = dna_to_kmers(full_sequence, kmer_size)
     result = newCountTable[uint64]()
     for k in convertedKmers.seeds:
         result.inc(uint64(k.kmer))
 
-proc countByChunk*(fai: Fai, chunk: Chunk, kmer_size: int=21):CountTableRef[uint64] =
+proc countByChunk(fai: Fai, chunk: Chunk, kmer_size: int=21):CountTableRef[uint64] =
     new (result)
     var sub_seq = fai.get(chunk.chrom_name, chunk.chrom_start, chunk.chrom_end)
     result = buildKmerCountTable(sub_seq, kmer_size)
@@ -55,7 +51,3 @@ proc mainRefCounter*(input_fn: string, svKmers: svIdx, kmer_size: int=21, chunk_
     for i in createdChunks(fai, chunk_size):
         let chunkCount = countByChunk(fai, i, kmer_size)
         addRefCount(chunkCount, svKmers)
-
-when isMainModule:
-    import cligen
-    dispatch(countRefKmers)
