c1f233b27c880f5a9931550938a8fd89a3d7e63e
peterkxie <peterkxie@yahoo.com>
2020-10-12 14:04:49 -0700

    chunk renaming + echo

--- src/nibpkg/refmers.nim
+++ src/nibpkg/refmers.nim
@@ -4,7 +4,7 @@ import tables
 import svidx
 
 type
-    Window* = object
+    Chunk* = object
         chrom_name: string
         chrom_start: int
         chrom_end: int
@@ -14,18 +14,18 @@ proc addRefCount*(refKmers: CountTableRef[uint64], svKmers: svIdx) =
         if svKmers.hasKey(k):
             svKmers[k].refCount += uint32(v)
 
-proc createChunk*(fai: Fai, window_size: int): seq[Window] =
-    var windowSeq = newSeq[Window]()
+proc createChunk*(fai: Fai, chunk_size: int): seq[Chunk] =
+    var chunkSeq = newSeq[Chunk]()
     for i in 0..<fai.len:
         let chrom_name = fai[i]
         let chrom_len = fai.chrom_len(chrom_name)
-        for j in countup(0, chrom_len, window_size):
-            var chunk: Window
+        for j in countup(0, chrom_len, chunk_size):
+            var chunk: Chunk
             chunk.chrom_name = chrom_name
             chunk.chrom_start = j
-            chunk.chrom_end = j + window_size
-            windowSeq.add(chunk)
-    return windowSeq
+            chunk.chrom_end = j + chunk_size
+            chunkSeq.add(chunk)
+    return chunkSeq
 
 proc buildKmerCountTable*(full_sequence: string, kmer_size: int=21):CountTableRef[uint64] =
     let convertedKmers: pot_t = dna_to_kmers(full_sequence, kmer_size)
@@ -33,21 +33,23 @@ proc buildKmerCountTable*(full_sequence: string, kmer_size: int=21):CountTableRe
     for k in convertedKmers.seeds:
         result.inc(uint64(k.kmer))
 
-proc countByChunk*(fai: Fai, chunk: Window, kmer_size: int=21):CountTableRef[uint64] =
+proc countByChunk*(fai: Fai, chunk: Chunk, kmer_size: int=21):CountTableRef[uint64] =
     new (result)
     var sub_seq = fai.get(chunk.chrom_name, chunk.chrom_start, chunk.chrom_end)
     result = buildKmerCountTable(sub_seq, kmer_size)
 
-proc countRefKmers*(input_fn: string, kmer_size: int=21, window_size: int=1000000) =
+proc countRefKmers*(input_fn: string, kmer_size: int=21, chunk_size: int=1000000) =
     ##Walk over reference sequences and count kmers
     var fai: Fai
     if not fai.open(input_fn):
         quit "couldn't open fasta"
     
-    let chunks = createChunk(fai, window_size)
+    let chunks = createChunk(fai, chunk_size)
+    echo("before", chunks)
     for i in chunks:
-        let windowCount = countByChunk(fai, i, kmer_size)
-    
+        echo("in loop", i)
+        let chunkCount = countByChunk(fai, i, kmer_size)
+
 when isMainModule:
     import cligen
     dispatch(countRefKmers)
