9295367527907ebd0ad12213694d225948f69b2f
Christopher Dunn <cdunn@pacificbiosciences.com>
2020-10-13 13:44:30 -0500

    drop dequeues from kmers

--- src/nibpkg/kmers.nim
+++ src/nibpkg/kmers.nim
@@ -132,7 +132,8 @@ proc dna_to_kmers*(sq: Dna; k: int): pot_t =
     forward_kmer.strand = forward
     reverse_kmer.strand = reverse
 
-    var kmer_stack = deques.initDeque[seed_t](128)
+    #var kmer_stack = deques.initDeque[seed_t](128)
+    var kmer_stack = newSeqOfCap[seed_t](128)
 
     ##  lk is the length of the kmers being built on the fly. The variable n is the total number of
     var
@@ -164,8 +165,8 @@ proc dna_to_kmers*(sq: Dna; k: int): pot_t =
 
         if lk >= k:
             inc(n, 2)
-            deques.addLast(kmer_stack, forward_kmer)
-            deques.addLast(kmer_stack, reverse_kmer)
+            kmer_stack.add(forward_kmer)
+            kmer_stack.add(reverse_kmer)
             inc(forward_kmer.pos, 1)
             inc(reverse_kmer.pos, 1)
         inc(i)
@@ -177,8 +178,8 @@ proc dna_to_kmers*(sq: Dna; k: int): pot_t =
     i = 0
 
     while i < n:
-        kmers.seeds[i] = kmer_stack.popFirst()
-        #kmers.seeds[i] = kmer_stack[i] # would also be fine
+        #kmers.seeds[i] = kmer_stack.popFirst()
+        kmers.seeds[i] = kmer_stack[i] # would also be fine
         #echo format("[$#]->$#", i, kmers.seeds[i].kmer)
         inc(i)
 
