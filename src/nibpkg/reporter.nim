885a4cb0be6f81a35ee6060cff2755d005e52fae
Eric T. Dawson <eric.t.dawson@gmail.com>
2020-10-13 18:26:44 -0400

    [reporter] Add SVIDX ref/alt kmer counts to reporters output

--- src/nibpkg/reporter.nim
+++ src/nibpkg/reporter.nim
@@ -6,7 +6,7 @@ import svidx
 ## N.B.: Add a function that takes a BAM path and returns the sample name
 ## 
 ## TODO: Add a function that handles genotypes using the svIdx's ref/alt count fields.
-proc report*(vcf_name : string, sv_read_supports : CountTableRef[uint32], sv_idex : svIdx, sample_name : string="SAMPLE") =
+proc report*(vcf_name : string, sv_read_supports : CountTableRef[uint32], sv_index : svIdx, sample_name : string="SAMPLE") =
     ## Query SV supports for each SV in a VCF, appending the sample name to a field in the INFO fileds if
     ## the SV is present in the sample (i.e., SV support count > 1)
     var variants:VCF
@@ -17,6 +17,9 @@ proc report*(vcf_name : string, sv_read_supports : CountTableRef[uint32], sv_ide
     ## Note: this will overwrite the existing entry if any exist in the VCF
     discard variants.header.add_info("NIB_SAMPLES_WITH_SV", ".", "String", "Sample name is present if SV is present in sample.")
     discard variants.header.add_info("NIB_READ_SUPPORTS", ".", "Integer", "The number of reads supporting a given SV.")
+    discard variants.header.add_info("NIB_SV_REF_KMERIDX_COUNT", "1", "Integer", "Number of REF kmers in SV index for SV.")
+    discard variants.header.add_info("NIB_SV_ALT_KMERIDX_COUNT", "1", "Integer", "Number of ALT kmers in SV index for SV.")
+
     #discard variants.header.add_info("NIB_ALT_SUPPORTS", ".", "Integer", "The number of reads supporting a given SV alt.")
     outputVCF.copy_header(variants.header)
     discard outputVCF.write_header()
@@ -25,9 +28,14 @@ proc report*(vcf_name : string, sv_read_supports : CountTableRef[uint32], sv_ide
     var sv_id :uint32= 0
     for v in variants:
         var sv_support_count = sv_read_supports.getOrDefault(sv_id, -1)
+        var sv_ref_k_count = sv_index[sv_id].refCount.int
+        var sv_alt_k_count = sv_index[sv_id].altCount.int
+        doAssert v.info.set("NIB_SV_REF_KMERIDX_COUNT", sv_ref_k_count) == Status.OK
+        doAssert v.info.set("NIB_SV_ALT_KMERIDX_COUNT", sv_alt_k_count) == Status.OK
         if sv_support_count > 0:
             doAssert v.info.set("NIB_SAMPLES_WITH_SV", sample_name) == Status.OK
             doAssert v.info.set("NIB_READ_SUPPORTS", sv_support_count) == Status.OK
+
 
         doAssert outputVCF.write_variant(v)
 
