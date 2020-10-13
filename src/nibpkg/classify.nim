c3e0fa637f7dda38427010fcecf6edc246d45ed6
Eric T. Dawson <eric.t.dawson@gmail.com>
2020-10-12 20:20:24 -0400

    [classify] Fix lint bugs, but still suspicious of the countTable instantiation.

--- src/nibpkg/classify.nim
+++ src/nibpkg/classify.nim
@@ -3,8 +3,9 @@ import tables
 import hts
 import ./mainLookup
 import ./read
+import ./svidx
 
-proc classify_bam(filename : string, idx : svIdx, k:int=25): CountTableRef =
+proc classify_bam(filename : string, idx : svIdx, k:int=25): CountTableRef[uint32, uint32] =
     result = newCountTable[uint32, uint32]()
 
     var bamfile:Bam
@@ -12,17 +13,20 @@ proc classify_bam(filename : string, idx : svIdx, k:int=25): CountTableRef =
 
     for record in bamfile:
         var read_classification = process_read(record.sequence, idx, k)
-        var filtered_classification = filter_read_matches(read_classification)
-        for key, val in filtered_classification.compatible_SVs:
+        filter_read_matches(read_classification)
+        for key, val in read_classification.compatible_SVs:
             result.mgetOrPut(key, val)
 
 
-proc classify_file(filename : string, idx : svIdx): CountTableRef =
+proc classify_file(filename : string, idx : svIdx, k:int=25): CountTableRef[uint32, uint32] =
     
     if endsWith(filename, ".bam"):
-        return classify_bam
+        return classify_bam(filename, idx, k)
     else:
         quit("Error: only BAM input currently supported.")
 
-proc main_classify(read_file : string, vcf_file : string, ref_file : string, k:int=25) =
-    
+proc main_classify(read_file : string, vcf_file : string, ref_file : string, k:int=25, flank:int=100) =
+
+    var idx : svIdx = buildSVIdx(ref_file, vcf_file, flank, k)
+
+    var svCounts: CountTableRef[uint32, uint32] = classify_file(read_file, idx, k)
